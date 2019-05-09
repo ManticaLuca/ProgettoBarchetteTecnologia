6bcb1b815422ffbf3c201c4e87ffa72bc19d8a30
davidemauri01 <dvd.mauri01@gmail.com>
2019-05-09 11:17:37 +0200

    Mettimi er core

---
+++ LaBarcaNellaVasca/src/barcaNellaVasca/Barchetta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barcaNellaVasca;
+
+/**
+ *
+ * @author Mauri_Davide
+ */
+public class Barchetta {
+    private float[] puntiX;
+    private float[] puntiY;
+    private float velocita;
+    
+    public Barchetta(float[] puntiX, float[] puntiY){
+        this.puntiX = puntiX;
+        this.puntiY = puntiY;
+        velocita = 0;        
+    }
+    
+}
